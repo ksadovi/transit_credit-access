11febb16cb8609b06d9acadb2a49a6c698a51858
Kyra Sadovi <kmsadovi@gmail.com>
2024-12-17 11:41:43 -0600

    Reorganizing for max readability

---
+++ code/clean_LODES/wrangling.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+library(data.table)
+
+aux  = fread("data/LODES/raw_files/dc_od_aux_JT04_2021.csv") %>% as_tibble
+main = fread("data/LODES/raw_files/dc_od_main_JT04_2021.csv") %>% as_tibble
+
+crosswalk = fread("data/LODES/dc_xwalk.csv") %>% as_tibble
