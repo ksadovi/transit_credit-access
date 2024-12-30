a72b2dd3387aa06253ed235a70bd8ad3de8db37f
Kyra Sadovi <kmsadovi@gmail.com>
2024-12-30 14:21:26 -0500

    Creating one file to run all functions in library

---
+++ 2_code/function_library/run_all_functions.R
@@ -0,0 +1,6 @@
+all_files = list.files("2_code/function_library/")
+all_files = all_files[all_files != "run_all_functions.R"]
+
+for(i in all_files){
+  source(paste0("2_code/function_library/", i))
+}
