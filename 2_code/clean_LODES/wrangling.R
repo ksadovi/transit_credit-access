ec36d772b58bbf534e358181c47ab000bdc9782c
Kyra Sadovi <kmsadovi@gmail.com>
2024-12-30 13:49:20 -0500

    Renamed and refined initial LODES wrangling file

--- 2_code/clean_LODES/wrangling.R
+++
@@ -1,5 +0,0 @@
-source("code/packages+defaults.R")
-aux  = fread("data/LODES/raw_files/dc_od_aux_JT04_2021.csv") %>% as_tibble
-main = fread("data/LODES/raw_files/dc_od_main_JT04_2021.csv") %>% as_tibble
-
-crosswalk = fread("data/LODES/dc_xwalk.csv") %>% as_tibble
