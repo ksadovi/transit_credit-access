218e5fab129fd9b8c279c8786618be9d5c9aa6be
Kyra Sadovi <kmsadovi@gmail.com>
2024-12-30 13:47:46 -0500

    Adding census/mapping libraries, Census API key

--- 2_code/packages+defaults.R
+++ 2_code/packages+defaults.R
@@ -2,3 +2,10 @@ library(tidyverse)
 library(data.table)
 library(readxl)
 library(janitor)
+library(tidycensus)
+library(tmap)
+library(osrm)
+library(tmaptools)
+
+# Census Data API key
+# census_api_key("195869cb0610c91bd120c0c19a46f6560d3a3961", install = T)
