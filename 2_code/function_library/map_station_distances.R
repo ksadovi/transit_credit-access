f9a5b9463b21d02633c46ac4c1f6377b2042dc8a
Kyra Sadovi <kmsadovi@gmail.com>
2024-12-30 17:03:31 -0500

    Adding parameter to exclude lines

--- 2_code/function_library/map_station_distances.R
+++ 2_code/function_library/map_station_distances.R
@@ -2,41 +2,48 @@
 # nearest train station, the number of miles on foot to the nearest train station, and the closest train station by 
 # miles and by distance (they should be the same unless there's a very marginal case)
 
-map_station_distances = function(centroids, length = "all"){
+map_station_distances = function(centroids, length = "all", exclude = "none"){
   centroids = centroids
-  stations = fread("3_output/cleaned_data/station_polygons.csv")
+  if(exclude == "none"){
+    stations = fread("3_output/cleaned_data/station_polygons.csv")
+  } else{
+    stations = fread("3_output/cleaned_data/station_polygons.csv") %>% 
+      filter(lines_serviced != exclude)
+  }
   routing = data.frame()
   if(length == "all"){
     end = nrow(centroids)
   } else{
     end = length
   }
-  timing = system.time(for(i in c(1:end)){
-    stats = data.frame()
-    for(j in 1:length(stations$station)){
-      tryCatch(
-        {route = osrmRoute(
-          src = st_coordinates(centroids$geometry[i]), 
-          dst = stations[j, 1:2], 
-          osrm.profile = "foot"
-        )}, error = function(msg){
-          return(NA)
-        }
-      )
-      info = cbind(route$duration, route$distance*0.621371, stations$station[j])
-      stats = rbind(stats, info) 
+  timing = system.time(
+    for(i in c(1:end)){
+      stats = data.frame()
+      for(j in 1:length(stations$station)){
+        tryCatch(
+          {route = osrmRoute(
+            src = st_coordinates(centroids$geometry[i]), 
+            dst = stations[j, 1:2], 
+            osrm.profile = "foot"
+          )}, error = function(msg){
+            return(NA)
+          }
+        )
+        info = cbind(route$duration, route$distance*0.621371, stations$station[j])
+        stats = rbind(stats, info) 
+      }
       
-    }
-    colnames(stats) = c("minutes", "miles", "station_name")
-    stats$minutes = as.numeric(stats$minutes)
-    stats$miles = as.numeric(stats$miles)
-    min_dur = min(stats$minutes)
-    min_dist = min(stats$miles)
-    closest_dur = stats$station_name[which(stats$minutes == min_dur)]
-    closest_dist = stats$station_name[which(stats$miles == min_dist)]
-    tract = dmv_tract_centroids$GEOID[i]
-    info2 = cbind(tract, min_dur, min_dist, closest_dur, closest_dist)
-    routing = rbind(routing, info2)
+      # Formatting Output
+      colnames(stats) = c("minutes", "miles", "station_name")
+      stats$minutes = as.numeric(stats$minutes)
+      stats$miles = as.numeric(stats$miles)
+      min_dur = min(stats$minutes)
+      min_dist = min(stats$miles)
+      closest_dur = stats$station_name[which(stats$minutes == min_dur)]
+      closest_dist = stats$station_name[which(stats$miles == min_dist)]
+      tract = dmv_tract_centroids$GEOID[i]
+      info2 = cbind(tract, min_dur, min_dist, closest_dur, closest_dist)
+      routing = rbind(routing, info2)
   })
   return(list(routing, timing))
 }
