4a5370fbe40648131d126c909ac3dbd1c600b574
Kyra Sadovi <kmsadovi@gmail.com>
2025-07-05 15:55:36 -0300

    Date conversion made more simple

--- 2_code/1_function_library/update_stations.R
+++ 2_code/1_function_library/update_stations.R
@@ -1,21 +1,21 @@
 # Preliminaries  --------
 source("2_code/packages+defaults.R")
-path_in = "3_output/cleaned_data/LODES/"
+path_in = "3_output/1_cleaned_data/"
 path_out = "3_output/"
 
 # Aggregating station locations across systems  --------
-# I have collected this data by hand in the CSV that is called. I will continue to add 
-# to it as I incorporate more transit systems into this analysis. I am making this a 
-# function so that it can be called from any other file in this project, achieving easy 
-# updating for the stations whenever I add more.
+# I have collected this data by hand in CSVs housed in 1_data/2_station_geographies/. I 
+# will have one CSV per transit system I cover. I will continue to add to it as I incorporate 
+# more transit systems into this analysis. I am making this a function so that it can be 
+# called from any other file in this project, achieving easy updating for the stations whenever I add more.
 
 update_stations = function(){
   stations = data.frame()
-  for(i in list.files("1_data/station_geographies")){
-    stations = rbind(stations, fread(paste0("1_data/station_geographies/", i)))
+  for(i in list.files("1_data/2_station_geographies/", pattern = "*.csv")){
+    stations = rbind(stations, fread(paste0("1_data/2_station_geographies/", i)))
   }
   stations = stations %>% 
-    mutate(open_date = excel_numeric_to_date(as.numeric(as.character(open_date)), date_system = "modern"))
+    mutate(open_date = as.Date(stations$open_date, format = "%m/%d/%y"))
   # Here I am converting these coordinates to geometric points
   station_poly <- st_as_sf(stations, coords = c("longitude", "latitude"), 
                            crs = 4326, agr = "constant")
