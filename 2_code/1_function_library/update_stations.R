c0be434a1a084b400825e836007f5f13969d3df3
Kyra Sadovi <kmsadovi@gmail.com>
2025-07-05 16:30:36 -0300

    Gave station update file more descriptive name

--- 2_code/1_function_library/update_stations.R
+++ 2_code/1_function_library/update_stations.R
@@ -18,6 +18,6 @@ update_stations = function(){
   station_poly <- st_as_sf(stations, coords = c("longitude", "latitude"), 
                            crs = 4326, agr = "constant")
   
-  fwrite(stations, file = "3_output/1_cleaned_data/2_station_geographies/station_polygons.csv")
+  fwrite(stations, file = "3_output/1_cleaned_data/2_station_geographies/stations_timeline_comprehensive.csv")
   return(station_poly)
 }
