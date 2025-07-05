4ae459955df49f72dac07e412509ee0a2154dad6
Kyra Sadovi <kmsadovi@gmail.com>
2025-07-05 16:19:10 -0300

    Correcting file paths

--- 2_code/1_function_library/run_all_functions.R
+++ 2_code/1_function_library/run_all_functions.R
@@ -1,8 +1,8 @@
-all_files = list.files("2_code/function_library/")
+all_files = list.files("2_code/1_function_library/")
 all_files = all_files[all_files != "run_all_functions.R"]
 
 for(i in all_files){
-  source(paste0("2_code/function_library/", i))
+  source(paste0("2_code/1_function_library/", i))
 }
 
 try(rm(all_files, i)) %>% suppressWarnings()
