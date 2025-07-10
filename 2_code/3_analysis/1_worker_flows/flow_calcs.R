9b8b3686e9bb58e9aae94a6d612db3c9618609d3
Kyra Sadovi <kmsadovi@gmail.com>
2025-07-10 18:52:38 -0300

    I mistakenly included people who live and work in the same tract. Now excluding to get accurate flow counts

--- 2_code/3_analysis/1_worker_flows/flow_calcs.R
+++ 2_code/3_analysis/1_worker_flows/flow_calcs.R
@@ -17,7 +17,7 @@ flow_calcs = function(states=c("all")){
   }
   for(i in states){
     blocks = read_rds(paste0("3_output/1_cleaned_data/3_LODES/1_preaggregated_metros/worker_flows_blocks_", i, ".rds"))
-    # Residents  --------
+    # Outflows  --------
     # Here, I sum the number of people who live in each tract and go to work anywhere 
     # (including in their own home tract). Since I've included main and aux LODES files
     # in this, this often means that it includes people who live very far outside of the 
@@ -25,36 +25,27 @@ flow_calcs = function(states=c("all")){
     # indicating that one person's residence was in Delaware but they worked in Chicago. 
     # This type of observation isn't really of interest here for the purposes of urban
     # and spatial econ. I deal with this in later files. 
-    residents_main = blocks %>% 
-      mutate(h_tract = substr(h_geocode, start = 1, stop = 11) %>% as.factor, 
-             S000 = as.numeric(S000)) %>% subset(select = -c(w_geocode))
-    residents_tab = aggregate(S000 ~ h_tract + census_year, data = residents_main, FUN = sum) %>% 
-      mutate(residents = S000, tract = h_tract) %>% subset(select = -c(S000, h_tract))
-    
     # Outflows --------
     outflows_main = blocks %>% filter(h_geocode != w_geocode) %>% 
       mutate(h_tract = substr(h_geocode, start = 1, stop = 11) %>% as.factor, 
-           S000 = as.numeric(S000)) %>% subset(select = -c(w_geocode))
+             S000 = as.numeric(S000)) %>% subset(select = -c(w_geocode))
     outflows_tab = aggregate(S000 ~ h_tract + census_year, data = outflows_main, FUN = sum) %>% 
       mutate(outflows = S000, tract = h_tract) %>% subset(select = -c(S000, h_tract))
     
     # Inflows --------
-    inflows_main = blocks %>% 
+    inflows_main = blocks %>% filter(h_geocode != w_geocode) %>% 
       mutate(w_tract = substr(w_geocode, start = 1, stop = 11) %>% as.factor, 
              S000 = as.numeric(S000)) %>% subset(select = -c(h_geocode))
     inflows_tab = aggregate(S000 ~ w_tract + census_year, data = inflows_main, FUN = sum) %>% 
       mutate(inflows = S000, tract = w_tract) %>% subset(select = -c(S000, w_tract))
     
     # All flows  --------
-    flows = residents_tab %>% full_join(inflows_tab) %>% full_join(outflows_tab) %>% 
+    flows = inflows_tab %>% full_join(outflows_tab) %>% 
       mutate(inflows = ifelse(is.na(inflows), 0, inflows), 
-             residents = ifelse(is.na(residents), 0, residents), 
-             outflows = ifelse(is.na(outflows), 0, outflows)) %>%
+             outflows = ifelse(is.na(outflows), 0, outflows)) 
       # There are quite a few census blocks with 0 inflows -- this makes sense, these would be 
       # neighborhoods that are primarily residential where no one works. I'm not too concerned 
       # about this. 
-      mutate(out_scaled = outflows/residents, 
-             in_scaled = inflows/residents) 
     
     write_rds(x = flows, file = paste0("3_output/1_cleaned_data/3_LODES/2_workerflow_tabs/flows_tracts_", i, ".rds"))
   }
