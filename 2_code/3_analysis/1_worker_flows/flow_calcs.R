e05ae1f6a94e11350df1921d3b09e0d8e050a8f6
Kyra Sadovi <kmsadovi@gmail.com>
2025-07-10 13:16:58 -0300

    Aggregate num residents, inflow of workers, outflow of workers by tract

--- 2_code/3_analysis/1_worker_flows/flow_calcs.R
+++ 2_code/3_analysis/1_worker_flows/flow_calcs.R
@@ -4,80 +4,66 @@
 # Preliminaries  --------
 source("2_code/packages+defaults.R")
 
-il_dats = read_rds("3_output/1_cleaned_data/3_LODES/worker_flows_blocks_IL.rds") 
-
-## Crosswalks --------
-crosswalk = data.frame()
-for(i in list.files("1_data/3_LODES/2_crosswalks/", pattern = "*_xwalk.csv")){
-  crosswalk = rbind(crosswalk, fread(paste0("1_data/3_LODES/2_crosswalks/", i)))
+flow_calcs = function(states=c("all")){
+  if((states == c("all"))[1]){
+    states = c()
+    for(i in list.files("3_output/1_cleaned_data/3_LODES/1_preaggregated_metros/")){
+      stat = str_extract(i, "(?<=_)[^_\\.]+(?=\\.)")
+      states = append(states, stat)
+    }
+  } else{
+    if((states %in% state.abb) == F) 
+      stop("Error: You didn't error a correct state abbreviation or the string 'all'. Please check your argument inputs.")
+  }
+  for(i in states){
+    blocks = read_rds(paste0("3_output/1_cleaned_data/3_LODES/1_preaggregated_metros/worker_flows_blocks_", i, ".rds"))
+    # Residents  --------
+    # Here, I sum the number of people who live in each tract and go to work anywhere 
+    # (including in their own home tract). Since I've included main and aux LODES files
+    # in this, this often means that it includes people who live very far outside of the 
+    # metro. For example, the Illinois file contains at least one tract from Delaware 
+    # indicating that one person's residence was in Delaware but they worked in Chicago. 
+    # This type of observation isn't really of interest here for the purposes of urban
+    # and spatial econ. I deal with this in later files. 
+    residents_main = blocks %>% 
+      mutate(h_tract = substr(h_geocode, start = 1, stop = 11) %>% as.factor, 
+             S000 = as.numeric(S000)) %>% subset(select = -c(w_geocode))
+    residents_tab = aggregate(S000 ~ h_tract + census_year, data = residents_main, FUN = sum) %>% 
+      mutate(residents = S000, tract = h_tract) %>% subset(select = -c(S000, h_tract))
+    
+    # Outflows --------
+    outflows_main = blocks %>% filter(h_geocode != w_geocode) %>% 
+      mutate(h_tract = substr(h_geocode, start = 1, stop = 11) %>% as.factor, 
+           S000 = as.numeric(S000)) %>% subset(select = -c(w_geocode))
+    outflows_tab = aggregate(S000 ~ h_tract + census_year, data = outflows_main, FUN = sum) %>% 
+      mutate(outflows = S000, tract = h_tract) %>% subset(select = -c(S000, h_tract))
+    
+    # Inflows --------
+    inflows_main = blocks %>% 
+      mutate(w_tract = substr(w_geocode, start = 1, stop = 11) %>% as.factor, 
+             S000 = as.numeric(S000)) %>% subset(select = -c(h_geocode))
+    inflows_tab = aggregate(S000 ~ w_tract + census_year, data = inflows_main, FUN = sum) %>% 
+      mutate(inflows = S000, tract = w_tract) %>% subset(select = -c(S000, w_tract))
+    
+    # All flows  --------
+    flows = residents_tab %>% full_join(inflows_tab) %>% full_join(outflows_tab) %>% 
+      mutate(inflows = ifelse(is.na(inflows), 0, inflows), 
+             residents = ifelse(is.na(residents), 0, residents), 
+             outflows = ifelse(is.na(outflows), 0, outflows)) %>%
+      # There are quite a few census blocks with 0 inflows -- this makes sense, these would be 
+      # neighborhoods that are primarily residential where no one works. I'm not too concerned 
+      # about this. 
+      mutate(out_scaled = outflows/residents, 
+             in_scaled = inflows/residents) 
+    
+    write_rds(x = flows, file = paste0("3_output/1_cleaned_data/3_LODES/2_workerflow_tabs/flows_tracts_", i, ".rds"))
+  }
 }
+  
 
-# Here, I only want to keep the Census tract code ('trct') and the 2020 Census
-# Tabulation Block Code ('tabblk2020')
-crosswalk = crosswalk %>% subset(select = c(tabblk2020, trct)) %>% 
-  as_tibble %>% 
-  mutate(tabblk2020 = as.factor(tabblk2020), 
-         w_geocode = as.factor(tabblk2020), 
-         trct = as.factor(trct))
-
-# Need to align the name of the Census Block Code in the Crosswalk with the one in 
-# the actual dataset. 
-colnames(crosswalk) = c("h_geocode", "tract", "w_geocode")
-
-# Outflows  --------
-# Here, I sum the number of people who live in each tract and go to work anywhere 
-# (including in their own home tract). 
-outflows_main = DMVW_full_state_tracts %>% subset(select = c(h_geocode, S000)) %>% 
-  mutate(h_geocode = as.factor(h_geocode), 
-         S000 = as.numeric(S000))
-
-outflows_main = aggregate(S000 ~ h_geocode, data = outflows_main, FUN = sum) %>% 
-  left_join(crosswalk) %>% na.omit %>% subset(select = c(h_geocode, tract, S000)) # I'm losing a lot of data here. I think this is because it includes people who live or work outside the DMV, so it doesn't actually matter that much. But there are probably some marginal cases worth checking. 
-colnames(outflows_main) = c("geocode", "tract", "outflows")
-
-# Inflows  --------
-
-inflows_main = DMVW_full_state_tracts %>% subset(select = c(w_geocode, S000)) %>% 
-  mutate(w_geocode = as.factor(w_geocode), 
-         S000 = as.numeric(S000))
-
-inflows_main = aggregate(S000 ~ w_geocode, data = inflows_main, FUN = sum) %>% 
-  left_join(crosswalk, by = 'w_geocode') %>% subset(select = c(w_geocode, tract, S000))
-colnames(inflows_main) = c("geocode", "tract", "inflows")
-
-# Scale by population --------
-# The actual scaling happens in the next chunk, this is just grabbing population data
-population <- get_decennial(
-  geography = "tract",
-  variables = "P1_001N",
-  state = str_sub(list.files("1_data/3_LODES/2_crosswalks/", full.names = F), start = 1, end = 2) %>% toupper() %>% unique, 
-  year = 2020,
-  geometry = TRUE
-) %>% as_tibble %>% 
-  mutate(pop = as.numeric(value), 
-         GEOID = as.factor(GEOID)) %>% 
-  subset(select = c(GEOID, value))
-
-# All flows  --------
-
-flows = outflows_main %>% left_join(inflows_main) %>% 
-  mutate(inflows = ifelse(is.na(inflows), 0, inflows), 
-         outflows = ifelse(is.na(outflows), 0, outflows)) %>%
-  # Need to aggregate again because was disaggregated by census block before which is too fine 
-  aggregate(cbind(inflows, outflows) ~ tract, FUN = sum) %>% 
-  mutate(flows = inflows - outflows)
-colnames(flows) = c("GEOID", "inflows", "outflows","flows") 
-flows = flows %>% left_join(population) %>% 
-  mutate(out_scaled = outflows/value, 
-         in_scaled = inflows/value, 
-         flow_scaled = flows/value, 
-         population = value, 
-         tract = GEOID) %>% subset(select = -c(value, GEOID))
-
-fwrite(flows, file = "3_output/1_cleaned_data/3_LODES/worker_flows.csv")
 
 # Cleanup --------
 # Removing all objects generated by this file that won't be necessary elsewhere
-try(rm(i, population, crosswalk, inflows_main, outflows_main, DMVW_full_state_tracts, flows)) %>% 
+try(rm(i, population, crosswalk, inflows_main, residents_main, DMVW_full_state_tracts, flows)) %>% 
   suppressWarnings()
 
