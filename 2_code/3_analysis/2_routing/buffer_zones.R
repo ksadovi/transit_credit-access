360670d38abf1b233acb424b6f49b2227cd4ab08
Kyra Sadovi <kmsadovi@gmail.com>
2025-07-11 13:09:12 -0300

    I've figured out a better way to intersect

--- 2_code/3_analysis/2_routing/buffer_zones.R
+++ 2_code/3_analysis/2_routing/buffer_zones.R
@@ -2,21 +2,26 @@ source("2_code/packages+defaults.R")
 source("2_code/function_library/run_all_functions.R")
 
 # buffer of 50 miles around Raleigh city centre
-station_poly = update_stations()
-bf = st_buffer(station_poly$geometry %>% st_transform(2264), .25*5280) %>% st_transform(4326)
+station_poly = update_stations() %>% 
+  mutate(station_buffers = st_buffer(station_poly$geometry %>% st_transform(2264), .25*5280) %>% st_transform(4326)) %>% 
+  mutate(tract_intersections = map(
+    station_buffers,
+    ~ dmv_tracts %>%
+      filter(st_intersects(., .x, sparse = FALSE)) %>%
+      pull(GEOID)))
 
 # intersection with buffer as a polygon
-near_brookland <- ifelse(sf::st_intersects(dmv_tracts, bf, sparse = F), 
+near_brookland <- ifelse(sf::st_intersects(dmv_tracts, station_poly$station_buffers[which(station_poly$station == "Brookland-CUA")], sparse = F), 
                                  TRUE, 
                                  FALSE)
-dmv_tracts = dmv_tracts %>% mutate(near_bkld = near_brookland)
-dc_metro_tracts <- dmv_tracts[dc_boundary, ] %>% 
-  erase_water() 
+dmv_tracts = dmv_tracts %>% mutate(near_station = ifelse(GEOID %in% station_poly$tract_intersections, T, F))
+# dc_metro_tracts <- dmv_tracts[dc_boundary, ] %>% 
+#   erase_water() 
 ggplot() +
-  geom_sf(data = dmv_tracts, color = "black") + #, aes(fill = near_bkld)) +
+  geom_sf(data = dmv_tracts, color = "black", aes(fill = near_station)) +
   geom_sf(data = dc_boundary, fill = NA, color = "blue") +
   geom_sf(data = bf, fill = NA, color = "darkgreen") + 
-  geom_sf(data = station_poly, fill = "red", color = "red") +
+  geom_sf(data = station_poly, color = "purple") +
   theme_classic() +
   coord_sf(default_crs = sf::st_crs(4326),
            xlim = c(-77.17, -76.92),
