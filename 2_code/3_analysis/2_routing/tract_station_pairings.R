2be561b0a12f8f59bbc7c4fc79a0301d1c6e6b6a
Kyra Sadovi <kmsadovi@gmail.com>
2025-07-12 19:42:32 -0300

    Removed water area from maps, runs more quickly, automated map borders

--- 2_code/3_analysis/2_routing/tract_station_pairings.R
+++ 2_code/3_analysis/2_routing/tract_station_pairings.R
@@ -5,9 +5,10 @@ tract_station_pairings = function(transit_system, rads = c(0.25,1), map_title = 
   
   # Need to load the state geographies to identify which states we need to pull 
   # Census tracts from. 
-  usa = st_as_sf(map("state", fill = T, plot = F)) %>%
+  usa = st_as_sf(map("state", plot = F, fill = TRUE))%>%
     st_transform(4326) 
   
+  sf_use_s2(FALSE)
   # Updating stations using our function previously defined in eponymous file. 
   station_poly = update_stations() %>%
     st_join(usa) %>%
@@ -22,6 +23,7 @@ tract_station_pairings = function(transit_system, rads = c(0.25,1), map_title = 
            state = append(state.abb, "DC")[match(str_to_title(ID), append(state.name, "District Of Columbia"))]) %>% 
     subset(select = -c(ID))
   
+  sf_use_s2(T)
   # Grab tracts by state
   metro_tracts <- map_dfr(unique(station_poly$state), ~{
     tracts(.x, cb = TRUE, year = year)
@@ -47,16 +49,8 @@ tract_station_pairings = function(transit_system, rads = c(0.25,1), map_title = 
     st_drop_geometry() %>%
     mutate(stations_far = ifelse(stations_far == "NA", NA, stations_far))
   
-  metro_tracts <- metro_tracts %>%
-    left_join(tract_station_summary_small, by = "GEOID") %>%
-    left_join(tract_station_summary_big, by = "GEOID") %>%
-    mutate(station_proximity = ifelse(!is.na(stations_close), "<=0.25 miles", 
-                                      ifelse(!is.na(stations_far), "<=1 mile", ">1 mile"))) %>%
-    erase_water()
-  
-  write_rds(metro_tracts, file = "3_output/1_cleaned_data/2_station_geographies/tract_station_pairings.rds")
-  
-  # Let's calculate the ggplot coordinates we should use: 
+  # Let's calculate the ggplot coordinates we should use and filter the tracts 
+  # plotted to only include those affected and their immediate surroundings: 
   ggplot_borders = function(x, dim = "long"){
     stopifnot((dim %in% c("long", "lat")))
     dims = ifelse(dim == "long", 1, 2)
@@ -69,18 +63,40 @@ tract_station_pairings = function(transit_system, rads = c(0.25,1), map_title = 
     min = lapply(x, extreme_coords, extreme = min) %>% unlist %>% min
     return(c(min, max))
   }
+  
+  metro_tracts <- metro_tracts %>%
+    left_join(tract_station_summary_small, by = "GEOID") %>%
+    left_join(tract_station_summary_big, by = "GEOID") %>%
+    mutate(station_proximity = ifelse(!is.na(stations_close), "<=0.25 miles", 
+                                      ifelse(!is.na(stations_far), "<=1 mile", ">1 mile")))
+  
+  
+  long = ggplot_borders(metro_tracts$geometry[which(!is.na(metro_tracts$stations_far))], dim = "long")
+  lat = ggplot_borders(metro_tracts$geometry[which(!is.na(metro_tracts$stations_far))], dim = "lat")
+  coords_df = tibble(long, lat)
+  
+  affected_area = st_polygon(
+    list(
+      cbind(
+        coords_df$long[c(1,2,2,1,1)], 
+        coords_df$lat[c(1,1,2,2,1)])
+    )
+  ) %>% st_sfc(crs = st_crs(metro_tracts))
+  
+  affected_tracts <- metro_tracts[st_within(metro_tracts, affected_area, sparse = FALSE)[,1], ] %>%
+    erase_water(year = year)
+  
+  write_rds(metro_tracts, file = paste0("3_output/1_cleaned_data/2_station_geographies/", transit_system, 
+                                        "_tract_station_pairings.rds"))
+  
   plot = ggplot() +
-    geom_sf(data = metro_tracts, color = "black", aes(fill = station_proximity)) +
+    geom_sf(data = affected_tracts, color = "black", aes(fill = station_proximity)) +
     geom_sf(data = station_poly, color = "purple") +
     theme_classic() + 
-    coord_sf(default_crs = sf::st_crs(4326),
-             xlim = ggplot_borders(metro_tracts$geometry[which(!is.na(metro_tracts$stations_far))], dim = "long"),
-             ylim = ggplot_borders(metro_tracts$geometry[which(!is.na(metro_tracts$stations_far))], dim = "lat")) + 
     guides(fill = guide_legend(title = "Proximity to Closest Station")) + 
     theme(legend.position = "bottom") + 
     labs(title = map_title, subtitle = paste0("Transit System: ", transit_system, ", open stations as of ", year))
   
   ggsave(filename = paste0("3_output/2_figures/1_maps/1_station_geographies/", transit_system, ".png"), plot)
 }
-# buffer of 50 miles around Raleigh city centre
 
