8b1856e88506bb1632d0266dbcc5ae292416d944
Kyra Sadovi <kmsadovi@gmail.com>
2025-08-16 18:24:06 -0400

    Adding a bunch of packages and Java quality check

--- 2_code/packages+defaults.R
+++ 2_code/packages+defaults.R
@@ -9,6 +9,19 @@ library(osrm)
 library(tigris)
 library(sf)
 library(tmaptools)
+library(rJava)
+library(rJavaEnv)
+library(osmextract)
+library(raster)
+library(elevatr)
+###### allocate RAM memory to Java **before** loading the {r5r} library
+options(java.parameters = "-Xmx64G")
+# check version of Java currently installed (if any) 
+if(java_check_version_rjava() == F){
+  # install Java 21
+  java_quick_install(version = 21)
+}
+library(r5r)
 options(tigris_use_cache = TRUE)
-
+Sys.which(c("osmosis", "osmfilter", "osmconvert", "fake"))
 # Be sure to have a Census API key before running this code! 
