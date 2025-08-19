6b5ec6b503239ff5cbb9be6ae3dff39bfcd59f6d
Kyra Sadovi <kmsadovi@gmail.com>
2025-08-19 11:44:11 -0500

    Deleted old DMV-specific files, pre-R5R implementation

--- 2_code/1_function_library/run_all_functions.R
+++
@@ -1,8 +0,0 @@
-all_files = list.files("2_code/1_function_library/")
-all_files = all_files[all_files != "run_all_functions.R"]
-
-for(i in all_files){
-  source(paste0("2_code/1_function_library/", i))
-}
-
-try(rm(all_files, i)) %>% suppressWarnings()
