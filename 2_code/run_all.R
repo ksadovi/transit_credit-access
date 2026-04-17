443a74a30a4655886b695f10bafe1a8cac5d6169
Kyra Sadovi <kmsadovi@gmail.com>
2026-04-17 14:10:02 -0500

    Updating workflow for new LODES processing

--- 2_code/run_all.R
+++ 2_code/run_all.R
@@ -19,22 +19,34 @@ run_all = function(overwrite = F){
   source("2_code/2_cleaning/1_clean_station_geographies/update_stations.R")
   update_stations()
   
-  # Process the LODES origin-destination data
+  # Identify closest transit station to each Census tract 
+  source("2_code/3_analysis/2_routing/tract_station_pairings.R")
+  for(system in unique(all_stations$system)){
+    tract_station_pairings(transit_system = system, overwrite_all = overwrite)
+  }
+  
+  # Process the LODES origin-destination data for the above-identified Census tracts
   source("2_code/2_cleaning/3_clean_LODES/LODEing_data.R")
-  LODEing_data(states = "all", overwrite = overwrite)
+  download_lodes()
+  aggregate_lodes(overwrite = overwrite)
   
   # Calculate the worker flow numbers by Census tract, year, and state. 
   source("2_code/3_analysis/1_worker_flows/flow_calcs.R")
   flow_calcs(states = c("all"), overwrite = overwrite)
-  
-  # Identify closest transit station to each Census tract 
-  source("2_code/3_analysis/2_routing/tract_station_pairings.R")
-  tract_station_pairings(transit_system = "WMATA")
 }
 
 run_all(overwrite = F)
 
-# This is pretty much what I've got for now. The next step is standardizing the distance calculations analysis folder; 
-# I think this is going to be very region-specific so I'll probably have a bunch 
-# of state/system subfolders. Want to think about how to organize data wrangling vs image generation. Honestly 
-# probably "analysis" is way too broad of a folder but for now it works. 
+
+
+
+
+
+
+
+
+
+
+
+
+
